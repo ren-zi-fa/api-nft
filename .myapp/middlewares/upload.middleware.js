af2bf081be90f552a341a91a3a4560e1de107f60
renzi febriandika <renzifebriandika923@gmail.com>
2025-04-26 17:28:26 +0700

    feat: initialize project structure with Express, middleware, and environment configuration

---
+++ .myapp/middlewares/upload.middleware.js
@@ -0,0 +1,36 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.upload = void 0;
+const multer_1 = __importDefault(require("multer"));
+const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
+// Pastikan direktori sudah ada
+const uploadDir = path_1.default.join(__dirname, "../../public/images");
+if (!fs_1.default.existsSync(uploadDir)) {
+    fs_1.default.mkdirSync(uploadDir, { recursive: true });
+}
+const storage = multer_1.default.diskStorage({
+    destination: (_req, _file, cb) => {
+        cb(null, uploadDir);
+    },
+    filename: (_req, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+exports.upload = (0, multer_1.default)({ storage });
+// import multer from "multer";
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "../../public/images/");
+//   },
+//   filename: (req, file, cb) => {
+//     const uniqueName = Date.now() + "-" + file.originalname;
+//     cb(null, uniqueName);
+//   },
+// });
+// const upload = multer({ storage });
+// export { upload };
+//# sourceMappingURL=upload.middleware.js.map
--- src/config/vars.ts
+++ src/config/vars.ts
@@ -1,15 +1,8 @@
 import path from "path";
-import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 
 // Gantikan __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Load .env file
-dotenv.config({
-  path: path.join(__dirname, "../../.env"),
-});
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 // Export config
 export default {
